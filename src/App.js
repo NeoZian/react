36ee1b47397682d9f772a359615e26ab904fc784
NeoZian <123123.sh14@gmail.com>
2023-08-02 02:39:35 +0600

    components:jsx

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee';
 
 class App extends Component {
   render() {
+    
+    console.log('we are about to list the employees');
+    const showEmployees = true;
+
+
     return (
       <div className="App">
+
         <div className="App-header">
         
           <h2>Welcome to React</h2>
         </div>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p>hello there i am salman</p>
         <p>we could do a lot better!!!couln't we??</p>
         <p>happy now?</p>
+
+        {showEmployees ? (
+
+        <div>
+
+        <Employee></Employee>
+        <Employee/>
+        <Employee/>
+
+        </div>
+        )
+        :(
+        <p>you cannot see the employees</p> )
+  }
       </div>
     );
   }
---
+++ src/components/Employee.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Employee(){
+    return <h3>here we got an employee</h3>;
+}
+
+export default Employee;
