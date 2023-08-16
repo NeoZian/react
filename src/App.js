91089a840540877c1d4b354c0a7549c81c1af436
NeoZian <123123.sh14@gmail.com>
2023-08-16 15:54:24 +0600

    useState-Hook

--- src/App.js
+++ src/App.js
@@ -1,46 +1,41 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 import Employee from './components/Employee';
 
-class App extends Component {
-  render() {
-    
-    console.log('we are about to list the employees');
-    const showEmployees = true;
+function App() {
+  console.log('we are about to list the employees');
+  const [role, setRole] = useState('dev');
+  const showEmployees = true;
 
-
-    return (
-      <div className="App">
-
-        <div className="App-header">
-        
-          <h2>Welcome to React</h2>
-        </div>
-
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hello there i am salman</p>
-        <p>we could do a lot better!!!couln't we??</p>
-        <p>happy now?</p>
-
-        {showEmployees ? (
-
-        <div>
-
-        <Employee name="jewel" role="Filing specialist"></Employee>
-        <Employee name="sal"/>
-        <Employee/>
-
-        </div>
-        )
-        :(
-        <p>you cannot see the employees</p> )
-  }
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Welcome to React</h2>
       </div>
-    );
-  }
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <p>hello there i am salman</p>
+      <p>we could do a lot better!!! couldn't we??</p>
+      <p>happy now?</p>
+      {showEmployees ? (
+        <Fragment>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <Employee name="jewel" role="Filing specialist" />
+          <Employee name="sal" role={role} />
+          <Employee />
+        </Fragment>
+      ) : (
+        <p>you cannot see the employees</p>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Employee(props){
 
-    //props.name = "something in my heart"; //cant change here in the child but can be done in parenta
+    //props.name = "something in my heart"; //cant change here in the child but can be done in parent
 
     return (
     <div>
